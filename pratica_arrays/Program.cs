935fd041fdfe242d5fa217e01bb1c6bcc819b757
Lucas-M7 <lucas015x@gmail.com>
2024-02-12 13:12:54 -0300

    Praticando arrays e for, foreach

---
+++ pratica_arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿// for (int i = 0; i <= 10; i++)
+// {
+//     Console.WriteLine($"{10} x {i} = {i * 1}");
+// }
+
+string[] nomesDosUsuarios = { "Lucas", "Edson", "Felipe", "Ítalo" };
+
+int contador = 0;
+
+foreach(string usuario in nomesDosUsuarios)
+{
+    Console.WriteLine($"Usuário N° {contador + 1} - {usuario}");
+    contador++;
+}
