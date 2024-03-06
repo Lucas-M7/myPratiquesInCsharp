7c34e77a1febbc85e7c36ed1d0d55eff68e938e7
Lucas Mateus <Lucas015x@gmail.com>
2024-03-06 12:22:41 -0300

    Up

--- Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
+++ Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Linq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7084bbbef4deb01113b26e751ddb454e9e88836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ff5d7eda143b9c507cd91f827d754af8a522d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Linq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Linq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
