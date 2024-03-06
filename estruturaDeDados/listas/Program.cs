74ff5d7eda143b9c507cd91f827d754af8a522d8
Lucas Mateus <Lucas015x@gmail.com>
2024-03-05 21:08:24 -0300

    up

---
+++ estruturaDeDados/listas/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> myList = new List<int>();
+List<int> myNewList = new List<int> { 20, 30, 40, 50, 60, 70 };
+
+// myList.AddRange(myNewList);
+// foreach (var itens in myList)
+// {
+//     Console.WriteLine(itens);
+// }
+
+// myList.Insert(0, 10);
+
+//  foreach (var itens in myList)
+//  {
+//      Console.WriteLine(itens);
+//  }
