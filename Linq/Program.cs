f44d6165f1bd78619a29f0c7095404fa69f9bf7f
Lucas Mateus <Lucas015x@gmail.com>
2024-03-14 15:58:07 -0300

    refactor: Descomentando o código

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,75 +1,52 @@
-﻿// using System.Security.Cryptography.X509Certificates;
-// using LinQ;
+﻿
+using LinQ;
 
-// var songs = new List<Songs>
-// {
-//     new Songs(id:1, name:"Enter Sandman", band:"Metallica", new DateTime(year:1991, month:8, day:7)),
-//     new Songs(id:2, name:"Eyes Without a Face", band:"Billy Idol", new DateTime(year:1984, month:6, day:12)),
-//     new Songs(id:3, name:"Sweet Child o' Mine", band:"Guns N' Roses", new DateTime(year:1988, month:6, day:21)),
-//     new Songs(id:4, name:"Highway To Hell", band:"AC/DC", new DateTime(year:1979, month:7, day:27)),
-// };
+var songs = new List<Songs>
+ {
+     new Songs(id:1, name:"Enter Sandman", band:"Metallica", new DateTime(year:1991, month:8, day:7)),
+     new Songs(id:2, name:"Eyes Without a Face", band:"Billy Idol", new DateTime(year:1984, month:6, day:12)),
+     new Songs(id:3, name:"Sweet Child o' Mine", band:"Guns N' Roses", new DateTime(year:1988, month:6, day:21)),
+     new Songs(id:4, name:"Highway To Hell", band:"AC/DC", new DateTime(year:1979, month:7, day:27)),
+ };
 
-// Console.WriteLine("Songs released in 1991:");
-// var songsReleasedIn91 = songs
-// .Where(x => x.ReleaseDate.Year == 1991)
-// .ToList();
+Console.WriteLine("Songs released in 1991:");
+var songsReleasedIn91 = songs
+.Where(x => x.ReleaseDate.Year == 1991)
+.ToList();
 
-// foreach(var datas in songsReleasedIn91)
-// {
-//     Console.WriteLine($"{datas}\n");
-// }
-
-// Console.WriteLine("Songs released on June:");
-// var songsReleasedInJune = songs
-// .Where(x => x.ReleaseDate.Month == 6);
-
-// foreach(var datas in songsReleasedInJune)
-// {
-//     Console.WriteLine($"{datas}");
-// }
-
-
-
-// Console.WriteLine("\nReturn songs in alphabetical order:");
-// var songsOrder = songs
-// .OrderBy(x => x.Name)
-// .ToList();
-
-// foreach(var item in songsOrder)
-// {
-//     Console.WriteLine($"{item}");
-// }
-
-
-// Console.WriteLine("\nLast two songs registred in alphabetical order:");
-// var songsOrderDesc = songs
-// .TakeLast(2)
-// .ToList();
-
-// foreach(var item in songsOrderDesc)
-// {
-//     Console.WriteLine($"{item}");
-// }
-
-
-
-
-//Three parts of a Linq  Query:
-// 1. Data Source.
-
-int[] numbers = [ 0, 1, 2, 3, 4, 5 ];
-
-//2. Query creation.
-// numQuery is an IEnumerable<int>
-
-var numQuery =
-    from num in numbers
-    where (num % 2) == 0
-    select num;
-
-//3. Query execution
-
-foreach (int num in numQuery)
+foreach (var datas in songsReleasedIn91)
 {
-    Console.Write(num);
+    Console.WriteLine($"{datas}\n");
+}
+
+Console.WriteLine("Songs released on June:");
+var songsReleasedInJune = songs
+.Where(x => x.ReleaseDate.Month == 6);
+
+foreach (var datas in songsReleasedInJune)
+{
+    Console.WriteLine($"{datas}");
+}
+
+
+
+Console.WriteLine("\nReturn songs in alphabetical order:");
+var songsOrder = songs
+.OrderBy(x => x.Name)
+.ToList();
+
+foreach (var item in songsOrder)
+{
+    Console.WriteLine($"{item}");
+}
+
+
+Console.WriteLine("\nLast two songs registred in alphabetical order:");
+var songsOrderDesc = songs
+.TakeLast(2)
+.ToList();
+
+foreach (var item in songsOrderDesc)
+{
+    Console.WriteLine($"{item}");
 }
--- Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
+++ Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Linq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe15ec467282e5896f06edd1e545f2a5b39d9d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162c6d3e514515f8123c02bd7278a06659e213dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Linq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Linq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
