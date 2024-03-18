421e6fb3dd7f94baff625ece828c79b6c0b14915
Lucas Mateus <Lucas015x@gmail.com>
2024-03-18 14:38:55 -0300

    feat: praticando o crudo com MongoDB

---
+++ Crud_comMongo/Program.cs
@@ -0,0 +1,12 @@
+﻿using Crud_comMongo.Services;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+var stringDeConexao = new MongoClient("mongodb+srv://lucas015x:silv4x0908@mycrud.xrmgle1.mongodb.net/");
+var bancoDeDados = stringDeConexao.GetDatabase("Crud");
+var colecao = bancoDeDados.GetCollection<BsonDocument>("Usuarios");
+
+
+CrudMongo documento = new();
+
+await documento.DeletarDocumento(colecao);
---
+++ Crud_comMongo/Services/CrudMongo.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Crud_comMongo.Services;
+
+public class CrudMongo
+{
+    public async Task CriarUsuario(IMongoCollection<BsonDocument> collection)
+    {
+        var documento = new BsonDocument
+        {
+            {"nome", "Ana Maria"},
+            {"idade", 21},
+            {"email", "Maria@email.com"}
+        };
+
+        await collection.InsertOneAsync(documento);
+        Console.WriteLine("Documento criado com sucesso!");
+    }
+
+    public async Task LerDocumentos(IMongoCollection<BsonDocument> collection)
+    {
+        var documentos = await collection.Find(new BsonDocument()).ToListAsync();
+        foreach (var documento in documentos)
+        {
+            Console.WriteLine(documento);
+        }
+    }
+
+    public async Task AtualizarDocumento(IMongoCollection<BsonDocument> collection)
+    {
+        var filtrar = Builders<BsonDocument>.Filter.Eq("nome", "Ana Maria");
+        var atualizar = Builders<BsonDocument>.Update.Set("nome", "Lucas Mateus")
+            .Set("idade", 18)
+            .Set("email", "Lucas@email.com");
+        await collection.UpdateOneAsync(filtrar, atualizar);
+        Console.WriteLine("Documento atualizado com sucesso!");
+    }
+
+    public async Task DeletarDocumento(IMongoCollection<BsonDocument> collection)
+    {
+        var filtrar = Builders<BsonDocument>.Filter.Eq("nome", "Ana Maria");
+        await collection.DeleteOneAsync(filtrar);
+        Console.WriteLine("Documento deletado com sucesso!");
+    }
+}
