9f761f10690d440483d10748bb3efa50a3f7e074
Lucas Mateus <Lucas015x@gmail.com>
2024-03-20 16:56:24 -0300

    feat: Treinando a lógica

---
+++ desafios-logica/fizzbuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿// Imprimir números de 1 a 100
+// Caso for múltiplo de 3 = Fizz, de 5 = Buzz, 3 e 5 = FizzBuzz
+// Caso não for múltiplo de nenhum dos dois, imprimir o número
+
+for (int i = 1; i <= 100; i++){
+  if (i % 3 == 0){
+        Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0){
+        Console.WriteLine("Buzz");
+    }
+    else if(i % 3 == 0 && i % 5 == 0){
+        Console.WriteLine("FizzBuzz");
+    }
+    else{
+        Console.WriteLine(i);
+    }
+}
---
+++ desafios-logica/numeroPrimo/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program {
+    static bool IsPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.Sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    static void Main() {
+        Console.WriteLine(IsPrime(7));
+        Console.WriteLine(IsPrime(15));
+    }
+}
