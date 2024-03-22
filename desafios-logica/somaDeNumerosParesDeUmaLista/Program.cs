a9b218dc7b476e207ef79db5816e85d681768a38
Lucas Mateus <Lucas015x@gmail.com>
2024-03-22 17:19:40 -0300

    feat: Somando todos os numeros pares de uma lista por ele mesmo

---
+++ desafios-logica/somaDeNumerosParesDeUmaLista/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> numeros =[1, 2, 4, 8, 16, 24];
+
+Console.WriteLine("Soma de todos os números pares da lista.\n");
+foreach (var item in numeros)
+{
+    if (item % 2 == 0)
+    {
+        Console.WriteLine($"{item} + {item} = {item + item}");
+    }
+    else
+    {
+        Console.WriteLine($"O número {item} é ímpar.");
+    }
+}
