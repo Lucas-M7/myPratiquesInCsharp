89c89e741e42ba1eb94241f22a964042d97aecaa
Lucas Mateus <Lucas015x@gmail.com>
2024-03-23 18:37:33 -0300

    feat: desafio feito

---
+++ desafios-logica/MaiorEMenorNumero/Program.cs
@@ -0,0 +1,20 @@
+﻿static void EncontrarMaiorEMenor(int[] lista) {
+    if(lista == null) {
+        Console.WriteLine("Lista Vazia");
+    } else {
+        int menor = lista[0], maior = lista[0];
+
+        foreach (var numero in lista) {
+            if (numero < menor) {
+                menor = numero;
+            } else if (numero > maior) {
+                maior = numero;
+            } else {
+                Console.WriteLine($"{menor}, {maior}");
+            }
+        }
+    }
+}
+
+int[] listaNumero = [74, -74, 748, 0, 1, 473, 74];
+EncontrarMaiorEMenor(listaNumero);
