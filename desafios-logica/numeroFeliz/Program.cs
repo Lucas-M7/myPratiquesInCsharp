2677559694bba6f79620f19557e0ad7cbc1e1873
Lucas Mateus <Lucas015x@gmail.com>
2024-04-05 14:12:14 -0300

    Desafio do Número Feliz

---
+++ desafios-logica/numeroFeliz/Program.cs
@@ -0,0 +1,27 @@
+﻿static int SquareSumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10; // The last digit extraction
+        sum += digit * digit;
+        number /= 10; // Remove the last digit
+    }
+    return sum;
+}
+
+static bool IsHappy(int number)
+{
+    HashSet<int> seen = [];
+    while (number != 1 && !seen.Contains(number))
+    {
+        seen.Add(number);
+        number = SquareSumOfDigits(number);
+    }
+
+    return number == 1;
+}
+
+int number = 19;
+bool result = IsHappy(number);
+Console.WriteLine($"The number {number} is {(result ? "happy" : "unhappy")}.");
