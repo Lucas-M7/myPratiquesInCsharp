818a180579b18aa44b90a5e6523b47fc7affd147
Lucas Mateus <Lucas015x@gmail.com>
2024-04-05 14:32:25 -0300

    Conhecendo o Hashset

---
+++ estruturaDeDados/HashSet/Program.cs
@@ -0,0 +1,10 @@
+﻿HashSet<string> names = [];
+
+names.Add("Lucas");
+names.Add("Fábio");
+names.Add("Kauã");
+names.Add("Guilherme");
+names.Add("Lucas"); // This element is ignored, because it already exists in ensemble
+
+string namesInLine = string.Join(", ", names);
+Console.WriteLine(namesInLine);
