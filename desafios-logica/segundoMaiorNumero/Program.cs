5e61c38edee0185256323cc43ccd392ed4ce657f
Lucas Mateus <Lucas015x@gmail.com>
2024-04-06 19:25:47 -0300

    descobrindo o segundo maior valor de um array

---
+++ desafios-logica/segundoMaiorNumero/Program.cs
@@ -0,0 +1,24 @@
+﻿static int GetTheSecondLargerValue(int[] myArr)
+{
+    int larger = int.MinValue;
+    int secondLarger = int.MinValue;
+
+    foreach (int item in myArr)
+    {
+        if (item > larger)
+        {
+            secondLarger = larger;
+            larger = item;
+        }
+        else if (item > secondLarger)
+        {
+            secondLarger = larger;
+        }
+    }
+    
+    return secondLarger;
+}
+
+int[] arr = [674, 724, 3894, 91, 0, -2];
+int secondLarger = GetTheSecondLargerValue(arr);
+Console.WriteLine($"The second larger value in array is: {secondLarger}");
