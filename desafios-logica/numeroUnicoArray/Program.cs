6d56518a6801dbc80a7c3cc6cb0a308253fa1d94
Lucas Mateus <Lucas015x@gmail.com>
2024-04-07 22:20:35 -0300

    Descobrindo um número único em uma lista e usando a opreação xor

---
+++ desafios-logica/numeroUnicoArray/Program.cs
@@ -0,0 +1,15 @@
+﻿static int SearchUniqueValueInArray(int[] numbers)
+{
+    int init = 0;
+
+    foreach (int num in numbers)
+    {
+        init ^= num; // Operação XOR
+    }
+
+    return init;
+}
+
+int[] myArr = [4, 4, 73, 73, 7, 9, 0, 0, 9];
+int uniqueNumber = SearchUniqueValueInArray(myArr);
+Console.WriteLine($"The unique number in list is: {uniqueNumber}");
