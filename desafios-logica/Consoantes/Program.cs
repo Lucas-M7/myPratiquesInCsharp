83efff8a86e9206770ce3f261306979f28632f11
Lucas Mateus <Lucas015x@gmail.com>
2024-04-10 19:22:14 -0300

    Desafio concluído

---
+++ desafios-logica/Consoantes/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Writte anything:");
+#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+#pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
+string message = Console.ReadLine().ToLower();
+#pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
+#pragma warning restore CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+
+int vogals = 0;
+int consonants = 0;
+
+foreach (char c in message)
+{
+    if (char.IsLetter(c))
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            vogals++;
+        else
+            consonants++;
+    }
+}
+
+Console.WriteLine($"Number of vogals: {vogals}");
+Console.WriteLine($"Number of consonants: {consonants}");
