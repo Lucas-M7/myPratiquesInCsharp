29bb1433b4fd6fe4ac961de1d2b5fd5906b12732
Lucas Mateus <Lucas015x@gmail.com>
2024-04-12 21:59:24 -0300

    Desafio concluído

---
+++ desafios-logica/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine($"The first numbers of Fibonacci sequence:");
+
+PrintFibonacciSequence(10);
+
+static void PrintFibonacciSequence(int n) // n é o número de elemento que a função vai gerar
+{
+    int first = 0;
+    int second = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"{first}");
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+}
