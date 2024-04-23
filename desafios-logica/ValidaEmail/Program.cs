9c099e119cf477d8d9cbaa70cf3be65fdf3b5a68
Lucas Mateus <Lucas015x@gmail.com>
2024-04-23 18:03:07 -0300

    ValidacaoDeEmailComRegex

---
+++ desafios-logica/ValidaEmail/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Registre um e-mail");
+string email = Console.ReadLine();
+
+if (IsValidEmail(email))
+{
+    Console.WriteLine("E-mail registrado.");
+}
+else
+{
+    Console.WriteLine("E-mail inválido.");
+}
+
+static bool IsValidEmail(string email)
+{
+    string pattern = @"^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+    Regex regex = new(pattern);
+
+    return regex.IsMatch(email);
+}
