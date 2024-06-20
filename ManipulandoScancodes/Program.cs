984ba11cd6419a591ec24b520dffcd51dc2e0871
Lucas Mateus <Lucas015x@gmail.com>
2024-06-20 19:06:34 -0300

    Conhecendo e manipulando scancodes

---
+++ ManipulandoScancodes/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<ConsoleKey, ConsoleKey> keyMap = new Dictionary<ConsoleKey, ConsoleKey>
+{
+    { ConsoleKey.L, ConsoleKey.U },
+    { ConsoleKey.U, ConsoleKey.C },
+    { ConsoleKey.C, ConsoleKey.A },
+    { ConsoleKey.A, ConsoleKey.S },
+    { ConsoleKey.S, ConsoleKey.M }
+};
+
+Console.WriteLine("Pressione ESC para sair.");
+
+while (true)
+{
+    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+    ConsoleKey originalKey = keyInfo.Key;
+
+    if (keyMap.ContainsKey(originalKey)) {
+
+        ConsoleKey mappedKey = keyMap[originalKey];
+        Console.WriteLine(mappedKey);
+    }
+    else {
+
+        Console.WriteLine($"Tecla pressionada: {originalKey}");
+    }
+
+    if (originalKey == ConsoleKey.Escape) {
+
+        break;
+    }
+}
